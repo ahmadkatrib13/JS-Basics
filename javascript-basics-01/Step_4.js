79d3b601931a393bac70673779c1afa07fce9866
Ahmad Katrib <ahmadkatrib123@gmail.com>
2021-11-24 14:32:38 +0200

    javascript-basics-01 Step_4

---
+++ javascript-basics-01/Step_4.js
@@ -0,0 +1,10 @@
+let name    =   window.prompt("Please Enter Your Name !!")      || "no Name";
+let surname =   window.prompt("Please Enter Your surname !!")   || "no Surname";
+let city    =   window.prompt("Please Enter Your city !!")      || "no City";
+
+
+alert(`
+        Nom : ${name}
+        prénom : ${surname}
+        Ville : ${city}
+`);
