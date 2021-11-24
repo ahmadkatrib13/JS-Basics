e45ff84d2a15c3fd2fa212ae9c87231b47549b64
Ahmad Katrib <ahmadkatrib123@gmail.com>
2021-11-24 15:04:23 +0200

    javscript-basics-01 Step_6

---
+++ javascript-basics-01/Step_6.js
@@ -0,0 +1,4 @@
+let nb1 =   window.prompt("give me the first number !!");
+let nb2 =   window.prompt("give me the second number !!");
+
+alert("the reminder of devision of "+nb1+" by "+nb2+" = "+nb1%nb2);
