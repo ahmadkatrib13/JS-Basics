88a74d58aabbf4ddec1b670b7e5b8b1ccfb86284
Ahmad Katrib <ahmadkatrib123@gmail.com>
2021-11-24 13:49:39 +0200

    javascript-basics-01 Step_2

---
+++ javascript-basics-01/Step_2.js
@@ -0,0 +1,11 @@
+let name, surname , city ;
+
+name    = "Doe";
+surname = "John";
+city    = "New-York";
+
+alert(`
+        Nom : ${name}
+        prénom : ${surname}
+        Ville : ${city}
+`);
