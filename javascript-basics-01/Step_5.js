e543528284793777c8c9a460a5e170574b24c5e7
Ahmad Katrib <ahmadkatrib123@gmail.com>
2021-11-24 15:01:50 +0200

    javscript-basics-01 Step_5

---
+++ javascript-basics-01/Step_5.js
@@ -0,0 +1,4 @@
+let nb1 =   window.prompt("give me the first number !!");
+let nb2 =   window.prompt("give me the second number !!");
+
+alert("the result  of multiplication of "+nb1+" by "+nb2+" = "+nb1*nb2);
