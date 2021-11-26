714389fae052877c0ac365d93f7360c85e970ca6
Ahmad Katrib <ahmadkatrib123@gmail.com>
2021-11-26 10:25:28 +0200

    edit javscript-basics-01 Step_5

--- javascript-basics-01/Step_5.js
+++ javascript-basics-01/Step_5.js
@@ -1,4 +1,10 @@
-let nb1 =   window.prompt("give me the first number !!");
-let nb2 =   window.prompt("give me the second number !!");
+const firstNumber      = document.getElementById("first_number");
+const secondNumber   = document.getElementById("second_number");
+const validate      = document.getElementById("validate");
 
-alert("the result  of multiplication of "+nb1+" by "+nb2+" = "+nb1*nb2);
+validate.addEventListener("click",function(){
+    // e.preventDefault();
+    let firstValue = firstNumber.value || 0;
+    let secondValue = secondNumber.value || 0;
+    alert("the result  of multiplication of "+firstValue+" by "+secondValue+" = "+firstValue*secondValue);
+});
