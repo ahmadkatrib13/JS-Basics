c2683df767b43f96e2f1c3be0d0f304cadfb0f40
Ahmad Katrib <ahmadkatrib123@gmail.com>
2021-11-28 21:42:37 +0200

    edit javascript-basics-02

--- javascript-basics-02/step 5/Step_5.js
+++ javascript-basics-02/step 5/Step_5.js
@@ -1,7 +1,11 @@
 let images = document.querySelectorAll("img");
+function changeimg(e, i) {
+    e.target.src = `images/image${i + 1}_2.jpg`;
+}
 
-images.forEach((img,key)=>{
-    img.onmouseover = function(){img.src = `images/image${key+1}_2.jpg`; };
+images.forEach((img, key) => {
+    img.addEventListener("mouseover", (e) => { changeimg(e, key); }
+    )
 });
 
 
